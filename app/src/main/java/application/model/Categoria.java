d1837fecb63ffca47f74a9e1ce298d7addd05364
Marco Aurélio <marcauremaza@gmail.com>
2024-10-11 01:38:22 +0000

    0.1 iniciais

---
+++ app/src/main/java/application/model/Categoria.java
@@ -0,0 +1,44 @@
+package application.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@table(name = "categorias")
+public class Categoria {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private long id;
+@Column(unique true, nullable = false)
+private String nome;
+
+@OneToMany (mappedBy "categoria")
+private Set<logo> jogos = new HashSet<>();
+
+public long getid() [
+
+return id:
+
+public void setId(long id) {
+
+this.id id;
+
+public String getNome() {
+
+return nome;
+
+public void setNome(String nome) ( this.nome nome;
+
+public Set<Jogo> get Jogos() {
+
+return jogos;
+
+public void set:logos (Set<logo jogos) ( this.jogos jogos;
